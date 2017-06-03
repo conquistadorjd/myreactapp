f1a431b89542cc436a10deacf3cd74af5c20fd79
Pravin <pravin22jd@gmail.com>
2017-06-03 22:59:32 +0530

    added details of components

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-var websiteName = 'techtrekking.net';
-var newStyle = { color:'blue'};
-
-var myList = (<div>
-                <h1>You are on website {websiteName}</h1>
+class App extends React.Component {
+   render() {
+      return (<div>  
+                <h1>You are on website techtrekking.net</h1>
                 <ul>
-                    <li style={newStyle}>FIrst</li>
+                    <li >FIrst</li>
                     <li>Second</li>
                     <li>Third</li>
                   </ul>
               </div>)
+   }
+}
 
-ReactDOM.render(myList, document.getElementById('root'))
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
